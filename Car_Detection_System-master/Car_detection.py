d793ae7034fdcce87e9c1f94524a08cca6d1a884
castolGodal <aryanpuri0711@gmail.com>
2022-05-02 20:31:38 +0530

    Update Car_detection.py

--- Car_Detection_System-master/Car_detection.py
+++ Car_Detection_System-master/Car_detection.py
@@ -1,4 +1,8 @@
 import cv2
+import random
+CAR_NUMBER  = random.randint(1111,9999)
+
+
 #install pythonCv2 by pip command 
 cap = cv2.VideoCapture('c:\\Users\\soura\\Desktop\\aryan\\carr_detection\\Car_Detection_System-master\\carv2.mp4')
 car_cascade = cv2.CascadeClassifier("c:\\Users\\soura\\Desktop\\aryan\\carr_detection\\Car_Detection_System-master\\cars.xml")
@@ -12,8 +16,8 @@ while True:
         plate = frames[y:y + h, x:x + w]
         cv2.rectangle(frames,(x,y),(x +w, y +h) ,(51 ,51,255),2)
         cv2.rectangle(frames, (x, y - 40), (x + w, y), (51,51,255), -2)
-        cv2.putText(frames, 'Car', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-        cv2.imshow('car',plate)
+        cv2.putText(frames, "car"  , (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+        cv2.imshow( "car" ,plate)
 
 
     frames = cv2.resize(frames,(600,400))
